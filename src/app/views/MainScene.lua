be1bc0c94dffe3b020e3ed02a3bb22ccdd03694b
Vincent <andyli386@gmail.com>
2018-08-03 16:17:03 +0800

    Add game handle

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -10,36 +10,69 @@ local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
 
-local function createStaticButton(node, imageName, x, y, pressed, released)
+local function createStaticButton(node, imageName, x, y, pressed)
     local button = ccui.Button:create()
-    button:loadTextureNormal(imageName, ccui.TextureResType.plistType)
-
-    local listener = cc.EventListenerTouchOneByOne:create()
-    listener:registerScriptHandler(pressed, cc.Handler.EVENT_TOUCH_BEGAN)
-    listener:registerScriptHandler(released, cc.Handler.EVENT_TOUCH_ENDED)
-    local eventDispatcher = button:getEventDispatcher()
-    eventDispatcher:addEventListenerWithSceneGraphPriority(listener, button)
+    button:loadTextureNormal(imageName .. ".png", ccui.TextureResType.plistType)
 
     button:move(x, y)
     button:addTo(node)
+    button:addTouchEventListener(pressed)
 
-    --listen:registerScriptHandler(touchMoved,cc.Handler.EVENT_TOUCH_MOVED)
-    --listen:registerScriptHandler(touchCanceled,cc.Handler.EVENT_TOUCH_CANCELLED)
- 
-    --节点添加的先后顺序为优先级的添加监听器方式，节点越上层，优先级越高
-    --eventDispatcher:addEventListenerWithSceneGraphPriority(listen,layer)
-    --void EventDispatcher::addEventListenerWithFixedPriority(EventListener* listener, int fixedPriority)
-    --固定优先级的添加监听器方式,fixedPriority>0, 数值越小，优先级越高
-    --eventDispatcher:addEventListenerWithFixedPriority(listener, node)
+end
 
+function MainScene:createDirectionButtons()
+    local direcRelate = {
+        up = { display.cx/4, display.cy/2+30,},
+        down = { display.cx/4, display.cy/2-93 },
+        left = { display.cx/4-60, display.cy/2-33 },
+        right = { display.cx/4+60, display.cy/2-33},
+    }
+
+    for key, value in pairs(direcRelate) do
+        createStaticButton(self, key, value[1], value[2], function(sender, eventType)
+            if eventType ==  ccui.TouchEventType.began then
+                if self.tank ~= nil then
+                    self.tank:MoveBegin(key)
+                    print(key .. " MoveBegin")
+                    return true
+                end
+            elseif eventType ==  ccui.TouchEventType.ended then
+                if self.tank ~= nil then
+                    self.tank:MoveEnd(key)
+                    print(key .. " MoveEnd")
+                    return true
+                end
+            end
+        end)
+    end
+end
+
+function MainScene:createOtherButtons()
+    local otherRelate = {
+        increase = {display.cx/4+780, display.cy/2+5,},
+        decrease = { display.cx/4+750, display.cy/2-32,},
+    }
+
+    for key, value in pairs(otherRelate) do
+        createStaticButton(self, key, value[1], value[2], function(sender, eventType)
+            if eventType ==  ccui.TouchEventType.began then
+                if key == "increase" then
+                    self.factory:SpawnRandom()
+                elseif key == "decrease" then
+                    self.tank:Fire()
+                end
+            end
+        end)
+    end
+end
+
+function MainScene:createHandleButton()
+    self:createDirectionButtons()
+    self:createOtherButtons()
 end
 
 --function MainScene:onCreate()
 function MainScene:ctor()
-    -- add background image
-    --display.newSprite("HelloWorld.png")
-    --    :move(display.center)
-    --    :addTo(self)
     local spriteFrameCache = cc.SpriteFrameCache:getInstance()
     spriteFrameCache:addSpriteFrames("res/tex1.plist")
 
@@ -55,102 +88,13 @@ function MainScene:ctor()
     self.map = Map.new(self)
 
     self.factory = Factory.new(self, self.map)
-    local size = cc.Director:getInstance():getWinSize()
 
+    --local size = cc.Director:getInstance():getWinSize()
     self.tank = PlayerTank.new(self, "tank_green", self.map, "player")
     self.tank:SetPos(7, 4)
 
-
     self:ProcessInput()
-
-    --local spriteFrameCache = cc.SpriteFrameCache:getInstance()
-    local up_dir = "up.png"
-    createStaticButton(self, up_dir, display.cx/4, display.cy/2+30, function()
-        if self.tank ~= nil then
-            self.tank:MoveBegin("up")
-            --print("up MoveBegin")
-            return true
-        end
-    end, function() 
-        if self.tank ~= nil then
-            self.tank:MoveEnd("up")
-            --print("up MoveEnd")
-            return true
-        end
-    end)
-
-    local down_dir = "down.png"
-    createStaticButton(self, down_dir, display.cx/4, display.cy/2-93, function()
-        if self.tank ~= nil then
-            self.tank:MoveBegin("down")
-            --print("down MoveBegin")
-            return true
-        end
-    end, function() 
-        if self.tank ~= nil then
-            self.tank:MoveEnd("down")
-            --print("down MoveEnd")
-            return true
-        end
-    end)
-
-    local left_dir = "left.png"
-    createStaticButton(self, left_dir, display.cx/4-60, display.cy/2-33, function()
-        if self.tank ~= nil then
-            self.tank:MoveBegin("left")
-            --print("left MoveBegin")
-            return true
-        end
-    end, function() 
-        if self.tank ~= nil then
-            self.tank:MoveEnd("left")
-            --print("left MoveEnd")
-            return true
-        end
-    end)
-
-    local right_dir = "right.png"
-    createStaticButton(self, right_dir, display.cx/4+60, display.cy/2-33, function()
-        if self.tank ~= nil then
-            self.tank:MoveBegin("right")
-            --print("right MoveBegin")
-            return true
-        end
-    end, function() 
-        if self.tank ~= nil then
-            self.tank:MoveEnd("right")
-            --print("right MoveEnd")
-            return true
-        end
-    end)
-
-    local increase = "increase.png"
-    createStaticButton(self, increase, display.cx/4+730, display.cy/2+5, function()
-        if self.tank ~= nil then
-            self.tank:Fire()
-            --print("fire")
-            return true
-        end
-    end, function() 
-        if self.tank ~= nil then
-            --print("fire end")
-            return true
-        end
-    end)
-
-    local decrease = "decrease.png"
-    createStaticButton(self, decrease, display.cx/4+700, display.cy/2-32, function()
-        if self.tank ~= nil then
-            self.factory:SpawnRandom()
-            --print("SpawnRandom")
-            return true
-        end
-    end, function() 
-        if self.tank ~= nil then
-            --print("SpawnRandom end")
-            return true
-        end
-    end)
+    self:createHandleButton()
 end
 
 
--- src/app/views/TitleScene.lua
+++ src/app/views/TitleScene.lua
@@ -6,6 +6,69 @@ local Map = require("app.Map")
 
 local TitleScene = class("TitleScene", cc.load("mvc").ViewBase)
 
+local function createStaticButton(node, imageName, x, y, pressed)
+    local button = ccui.Button:create()
+    button:loadTextureNormal(imageName .. ".png", ccui.TextureResType.plistType)
+
+    button:move(x, y)
+    button:addTo(node)
+    button:addTouchEventListener(pressed)
+
+end
+
+function TitleScene:createDirectionButtons()
+    local direcRelate = {
+        up = { display.cx/4, display.cy/2+30,},
+        down = { display.cx/4, display.cy/2-93 },
+        left = { display.cx/4-60, display.cy/2-33 },
+        right = { display.cx/4+60, display.cy/2-33},
+    }
+
+    for key, value in pairs(direcRelate) do
+        createStaticButton(self, key, value[1], value[2], function(sender, eventType)
+            if eventType ==  ccui.TouchEventType.began then
+                if self.tank ~= nil and key == "left" then
+                    self.tank:SetPos(4, 1)
+                    return true
+                elseif self.tank ~= nil and key == "right" then
+                    self.tank:SetPos(11, 1)
+                    return true
+                end
+            end
+        end)
+    end
+end
+
+function TitleScene:createOtherButtons()
+    local otherRelate = {
+        increase = {display.cx/4+780, display.cy/2+5,},
+        decrease = { display.cx/4+750, display.cy/2-32,},
+    }
+
+    for key, value in pairs(otherRelate) do
+        createStaticButton(self, key, value[1], value[2], function(sender, eventType)
+            if eventType ==  ccui.TouchEventType.began then
+                local sceneName
+                local x, _ = self.tank:GetPos()
+                if x == 4 then
+                    sceneName = "app.views.MainScene"
+                else
+                    sceneName = "app.views.EditorScene"
+                end
+
+                local sceneLayer = require(sceneName)
+                local scene = sceneLayer.create()
+                display.runScene(scene, "fade", 0.6)
+            end
+        end)
+    end
+end
+
+function TitleScene:createHandleButton()
+    self:createDirectionButtons()
+    self:createOtherButtons()
+end
+
 function TitleScene:onCreate()
     local spriteFrameCache = cc.SpriteFrameCache:getInstance()
     spriteFrameCache:addSpriteFrames("res/tex1.plist")
@@ -17,6 +80,7 @@ function TitleScene:onCreate()
     self.tank:SetPos(4, 1)
 --
     self:ProcessInput()
+    self:createHandleButton()
 
 end
 
